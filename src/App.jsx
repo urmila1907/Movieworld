65fb0599d6adbba4763002fba5be3738cb2de961
urmila1907 <urmilasangwan744@gmail.com>
2023-07-25 21:30:07 +0530

    Created register component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.scss";
+import Register from "./pages/register/Register";
 // import Home from "./pages/home/Home";
-import Watch from "./pages/watch/Watch";
+// import Watch from "./pages/watch/Watch";
 
 function App() {
   return (
     <div>
-      <Watch/>
+      <Register/>
     </div>
   );
 }
