b1f81f2de0239f7363471b38cee1e0f2572454b1
urmila1907 <urmilasangwan744@gmail.com>
2023-07-25 21:34:25 +0530

    Created login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.scss";
-import Register from "./pages/register/Register";
-// import Home from "./pages/home/Home";
+// import Login from "./pages/login/Login";
+// import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
 // import Watch from "./pages/watch/Watch";
 
 function App() {
   return (
     <div>
-      <Register/>
+      <Home/>
     </div>
   );
 }
