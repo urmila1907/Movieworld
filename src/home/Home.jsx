000315017f0b0b3f1dcb7155872dd96081140593
urmila1907 <urmilasangwan744@gmail.com>
2023-07-25 21:07:01 +0530

    Created and styled list and listItem component

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import './home.scss'
 import Navbar from '../components/navbar/Navbar'
 import Featured from '../components/featured/Featured'
+import List from '../components/list/List'
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <Featured/>
+      <Featured />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
   );
 }
